02e4774befcfcb009c188e951bd939207945bc1a
raag <raagpc@disroot.org>
2021-01-25 23:43:02 -0600

    first commit

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from '../components/navbar/Navbar'
+
+export const About = (props) => {
+    return (
+       <div className="-view">
+           <Navbar />
+           <h1>Esto es about</h1>
+       </div>
+    )
+}
+
+export default About
